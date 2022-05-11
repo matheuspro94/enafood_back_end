a7b13cb1252e7409b0b2877a3b6b39e8a5be8a07
mroenca40 <mproenca.marketing@gmail.com>
2022-05-11 12:01:24 -0300

    add repository app with model

--- src/data/products.js
+++ src/data/products.js
@@ -7,9 +7,15 @@ const products = [
   },
   {
     'name': 'x-burger',
-    'price': 11.99,
+    'price': 9.99,
     'quantity': 1,
     'itens': ['tomate', 'pão', 'Hamburquer', 'alface']
+  },
+  {
+    'name': 'x-bacon',
+    'price': 10.99,
+    'quantity': 1,
+    'itens': ['tomate', 'pão', 'Hamburquer', 'alface', 'bacon']
   },
 ]
 
